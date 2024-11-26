634f73842cef57c51d21044af9a189b6112b1a89
Eric L <60950660+elarswa@users.noreply.github.com>
2024-11-26 16:00:34 -0700

    feat: compose set up

---
+++ app.py
@@ -0,0 +1,28 @@
+import time
+
+import redis
+from flask import Flask
+
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try: 
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+@app.route('/')
+def hello():
+    count = get_hit_count()
+    return f'Hello from Docker! I have been seen {count} times.\n'
+
+@app.route('/clear')
+def clearCache():
+    cache.set('hits', 0)
+    return f'Hello! You have reset the visits count to 0 \n'
